6c91aeda2bb794ba69f10be6997bf6db284f5e71
rameez500 <rameez500@gmail.com>
2019-02-14 00:18:21 -0500

    Create view_table_2.py

---
+++ view_table_2.py
@@ -0,0 +1,15 @@
+from IPython.display import HTML
+
+def View2(df):
+    css = """<style>
+    table { border-collapse: collapse; border: 3px solid #eee; }
+    table tr th:first-child { background-color: #eeeeee; color: #333; font-weight: bold }
+    table thead th { background-color: #eee; color: #000; }
+    tr, th, td { border: 1px solid #ccc; border-width: 1px 0 0 1px; border-collapse: collapse;
+    padding: 3px; font-family: monospace; font-size: 10px }</style>
+    """
+    s  = '<script type="text/Javascript">'
+    s += 'var win = window.open("", "Title", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=780, height=200, top="+(screen.height-400)+", left="+(screen.width-840));'
+    s += 'win.document.body.innerHTML = \'' + (df.to_html() + css).replace("\n",'\\') + '\';'
+    s += '</script>'
+    return(HTML(s+css))

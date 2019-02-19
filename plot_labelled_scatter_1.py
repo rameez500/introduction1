973f1c2cb9553f90c1ad49d4c17caf88035d0437
rameez500 <rameez500@gmail.com>
2019-02-19 17:54:08 -0500

    Create plot_labelled_scatter_1.py

---
+++ plot_labelled_scatter_1.py
@@ -0,0 +1,36 @@
+def plot_labelled_scatter(X, y, class_labels):
+
+    import numpy
+    import pandas as pd
+    import seaborn as sn
+    import matplotlib.pyplot as plt
+    import matplotlib.cm as cm
+    from matplotlib.colors import ListedColormap, BoundaryNorm
+    from sklearn import neighbors
+    import matplotlib.patches as mpatches
+    import graphviz
+    from sklearn.tree import export_graphviz
+    import matplotlib.patches as mpatches
+
+    num_labels = len(class_labels)
+
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    marker_array = ['o', '^', '*']
+    color_array = ['#FFFF00', '#00AAFF', '#000000', '#FF00AA']
+    cmap_bold = ListedColormap(color_array)
+    bnorm = BoundaryNorm(numpy.arange(0, num_labels + 1, 1), ncolors=num_labels)
+    plt.figure()
+
+    plt.scatter(X[:, 0], X[:, 1], s=65, c=y, cmap=cmap_bold, norm = bnorm, alpha = 0.40, edgecolor='black', lw = 1)
+
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+
+    h = []
+    for c in range(0, num_labels):
+        h.append(mpatches.Patch(color=color_array[c], label=class_labels[c]))
+    plt.legend(handles=h)
+
+    plt.show()

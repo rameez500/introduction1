80ac81dd4c6528151f8029039ee3e4b28cd84798
rameez500 <rameez500@gmail.com>
2019-02-18 20:14:19 -0500

    Create plot_class_regions_for_classifier_subplot_1.py

---
+++ plot_class_regions_for_classifier_subplot_1.py
@@ -0,0 +1,56 @@
+def plot_class_regions_for_classifier_subplot(clf, X, y, X_test, y_test, title, subplot, target_names = None, plot_decision_regions = True):
+
+    import numpy
+    import pandas as pd
+    import seaborn as sn
+    import matplotlib.pyplot as plt
+    import matplotlib.cm as cm
+    from matplotlib.colors import ListedColormap, BoundaryNorm
+    from sklearn import neighbors
+    import matplotlib.patches as mpatches
+    import graphviz
+    from sklearn.tree import export_graphviz
+    import matplotlib.patches as mpatches
+
+    numClasses = numpy.amax(y) + 1
+    color_list_light = ['#FFFFAA', '#EFEFEF', '#AAFFAA', '#AAAAFF']
+    color_list_bold = ['#EEEE00', '#000000', '#00CC00', '#0000CC']
+    cmap_light = ListedColormap(color_list_light[0:numClasses])
+    cmap_bold  = ListedColormap(color_list_bold[0:numClasses])
+
+    h = 0.03
+    k = 0.5
+    x_plot_adjust = 0.1
+    y_plot_adjust = 0.1
+    plot_symbol_size = 50
+
+    x_min = X[:, 0].min()
+    x_max = X[:, 0].max()
+    y_min = X[:, 1].min()
+    y_max = X[:, 1].max()
+    x2, y2 = numpy.meshgrid(numpy.arange(x_min-k, x_max+k, h), numpy.arange(y_min-k, y_max+k, h))
+
+    P = clf.predict(numpy.c_[x2.ravel(), y2.ravel()])
+    P = P.reshape(x2.shape)
+
+    if plot_decision_regions:
+        subplot.contourf(x2, y2, P, cmap=cmap_light, alpha = 0.8)
+
+    subplot.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold, s=plot_symbol_size, edgecolor = 'black')
+    subplot.set_xlim(x_min - x_plot_adjust, x_max + x_plot_adjust)
+    subplot.set_ylim(y_min - y_plot_adjust, y_max + y_plot_adjust)
+
+    if (X_test is not None):
+        subplot.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cmap_bold, s=plot_symbol_size, marker='^', edgecolor = 'black')
+        train_score = clf.score(X, y)
+        test_score  = clf.score(X_test, y_test)
+        title = title + "\nTrain score = {:.2f}, Test score = {:.2f}".format(train_score, test_score)
+
+    subplot.set_title(title)
+
+    if (target_names is not None):
+        legend_handles = []
+        for i in range(0, len(target_names)):
+            patch = mpatches.Patch(color=color_list_bold[i], label=target_names[i])
+            legend_handles.append(patch)
+        subplot.legend(loc=0, handles=legend_handles)
